560d1c11dccaa48d50b1a11470fdacb64ca756d6
Ian Rasbheri <ian@irasbheri.lan>
2026-05-19 09:17:35 +0100

    Add option text to link

--- version.py
+++ version.py
@@ -1,7 +1,20 @@
 # version.py
-__version__ = "0.3.1"
+__version__ = "0.3.3"
 
-# Changlog
+# Changelog
 
-# v0.3.1
+# [0.3.3] - 2026-05-19
+# Set learner intial box height to 2 lines
+# Links can now have option text in front
+
+# [0.3.2] - 2026-05-18
 # Changed css from app.css to use the Falcon bootstrap theme
+
+# [0.3.1] - 2026-05-17
+# Learner slug hardcoded to /vm
+
+# [0.3.0] - 2026-05-16
+# Can now uplod any type of file - max 10MB
+
+# [0.2.0] - 2035-06-01
+# Initial release - limited to images only
